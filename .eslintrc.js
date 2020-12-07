5c751d7a2812f0e5b4bef739b114e796816c431d
Utkarsh Gupta <utkarshgupta137@gmail.com>
2020-12-07 07:25:13 +0530

    add js/react boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+const eslintrc = {
+  extends: [
+    "eslint:recommended",
+    "plugin:import/warnings",
+    "plugin:import/errors",
+    "plugin:promise/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "react-app",
+    "standard",
+    "airbnb",
+    "prettier",
+    "prettier/react",
+    "prettier/standard",
+  ],
+  rules: {
+    "arrow-body-style": ["error", "always"],
+    curly: ["error", "all"],
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+        },
+        groups: ["builtin", "external", "parent", "sibling"],
+        "newlines-between": "always",
+      },
+    ],
+    "no-console": "off",
+    "no-param-reassign": [
+      "error",
+      {
+        props: false,
+      },
+    ],
+    "react/prop-types": "off",
+  },
+};
+
+module.exports = process.env.DISABLE_ESLINT ? {} : eslintrc;
